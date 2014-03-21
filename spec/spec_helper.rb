30d43e5db9726676a6dedc2971a3591d144036fe
thai.huynh <thaihuynh@thaihuynh-Aspire.(none)>
2014-03-21 13:27:23 +0700

    Finish user edit, update, index, and destroy actions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,4 +40,17 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
   config.include Capybara::DSL
+  def sign_in(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.hash(remember_token))
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
+end
 end
